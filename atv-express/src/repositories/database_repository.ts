ebf8c0d9bbeecf10b55a659af3d9cd58b726fa4e
Igor julliano <igor.julliano2@gmail.com>
2023-06-08 00:35:49 -0300

    refactor: use sqlite params instead of string interpolation

--- atv-express/src/repositories/database_repository.ts
+++ atv-express/src/repositories/database_repository.ts
@@ -1,12 +1,12 @@
-import { open, Database } from "sqlite";
-import  sqlite3  from "sqlite3";
+import { open, Database } from 'sqlite';
+import sqlite3 from 'sqlite3';
 
 export default class DatabaseRepository {
-    private _database!: Database
+    private _database!: Database;
 
     static async initialize(name: string): Promise<DatabaseRepository> {
-        const repository: DatabaseRepository = new DatabaseRepository()
-        repository._database = await open({filename: name, driver: sqlite3.Database})
+        const repository: DatabaseRepository = new DatabaseRepository();
+        repository._database = await open({ filename: name, driver: sqlite3.Database });
 
         await repository._database.exec(`
         CREATE TABLE IF NOT EXISTS POST(
@@ -28,10 +28,10 @@ export default class DatabaseRepository {
     }
 
     get database() {
-        return this._database
+        return this._database;
     }
-    
+
     async finalizar(): Promise<void> {
-        await this._database.close()
+        await this._database.close();
     }
 }
